6d99ae51775926a1543452f534d5a1c5651086a6
chinelon <72803021+chinelon@users.noreply.github.com>
2023-11-08 14:27:13 +0100

    made menu,home and contact

--- src/home.js
+++ src/home.js
@@ -1,1 +1,19 @@
 //alert('my name is chinelo')
+const home = (() => {
+
+    const content = document.querySelector("#content")
+
+    const homepage = document.createElement("homepage")
+
+    homepage.innerHTML = `<div>
+    <p>HELLO THERE</p>
+    <img src="../assets/chilos-food.png" alt="Chilos Food Logo" style="align-items: center;">
+    <h1 style="color: rgb(110, 216, 110);">Chilos Food</h1>
+    <p>This Restaurant is great and it sells extremely healthy food stuff</p>
+    </div>`
+
+    content.appendChild(homepage)
+
+})()
+
+export default home;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import home from './home'
+console.log('the website is working')
+
+function myf(){
+    home()
+}
+
+myf()
